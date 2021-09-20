9ed2dd0e63e6050d8d6d69ba19dbbad883134282
Hrant Grishyan <grishyan.hrant@gmail.com>
2021-09-20 19:56:18 +0400

    20

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser").urlencoded({ extended: true });
+
+app.use(express.json());
+
+app.use(cors());
+
+const schema = mongoose.Schema({
+  title: String,
+});
+
+mongoose.connect(
+  "mongodb+srv://username:1234@cluster0.4nev9.mongodb.net/react_node", //texadreq aystex dzer dbn
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true }
+);
+
+let model = mongoose.model("text", schema);
+
+app.get("/", async (req, res) => {
+  let mymodel = await model.find({});
+  res.send(mymodel);
+});
+
+app.post("/create", bodyParser, async (req, res) => {
+  if (req.body.title !== 0) {
+    await model({
+      title: req.body.title,
+    }).save();
+    res.redirect("http://localhost:3000/");
+  } else {
+    res.send("error");
+  }
+});
+
+app.post("/deleteTodo", bodyParser, async (req, res) => {
+  await model.findByIdAndDelete(req.body.todoId);
+});
+
+app.listen(9000);
